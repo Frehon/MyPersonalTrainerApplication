8776670dc640cfdde5775f29a5339008c796309f
Frehon <tomasz.gogolewski@hotmail.com>
2016-11-04 18:57:42 +0100

    Main Panel Template Done.

--- app/src/main/java/pl/edu/pwr/a200184student/my_personal_trainer/controller/main_panel_controller.java
+++ app/src/main/java/pl/edu/pwr/a200184student/my_personal_trainer/controller/main_panel_controller.java
@@ -2,6 +2,7 @@ package pl.edu.pwr.a200184student.my_personal_trainer.controller;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -28,17 +29,6 @@ public class main_panel_controller extends AppCompatActivity
         setContentView(R.layout.main_panel_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
