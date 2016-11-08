a0786ece1c6abd4f3adc1d5b6837e1faa50e489e
Frehon <tomasz.gogolewski@hotmail.com>
2016-11-08 19:17:36 +0100

    coos

---
+++ app/src/main/java/pl/edu/pwr/a200184student/my_personal_trainer/controller/Registry_Detail_Controller.java
@@ -0,0 +1,8 @@
+package pl.edu.pwr.a200184student.my_personal_trainer.controller;
+
+/**
+ * Created by Tomasz Gogolewski on 08.11.2016.
+ */
+
+public class Registry_Detail_Controller {
+}
---
+++ app/src/main/java/pl/edu/pwr/a200184student/my_personal_trainer/util/Diet_Util.java
@@ -0,0 +1,8 @@
+package pl.edu.pwr.a200184student.my_personal_trainer.util;
+
+/**
+ * Created by Tomasz Gogolewski on 08.11.2016.
+ */
+
+public class Diet_Util {
+}
