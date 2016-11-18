53780c1a4424cceb427e805e514600225a810991
Frehon <tomasz.gogolewski@hotmail.com>
2016-11-18 23:20:53 +0100

    Meals expandableListView and adapter added.

--- app/src/main/java/pl/edu/pwr/a200184student/my_personal_trainer/Adapters/ExpandableListViewAdapter.java
+++ app/src/main/java/pl/edu/pwr/a200184student/my_personal_trainer/Adapters/ExpandableListViewAdapter.java
@@ -1,6 +1,8 @@
 package pl.edu.pwr.a200184student.my_personal_trainer.Adapters;
 
 
+import java.util.HashMap;
+import java.util.List;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
@@ -9,46 +11,25 @@ import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
-import java.util.HashMap;
-import java.util.List;
-
 import pl.edu.pwr.a200184student.my_personal_trainer.R;
 
-public class ExpandableListViewAdapter extends BaseExpandableListAdapter{
+public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
 
-    private List<String> meals ;
-    private HashMap<String,List<String>> mealItems;
     private Context context;
+    private List<String> meals;
+    private HashMap<String, List<String>> mealItems;
 
-    public ExpandableListViewAdapter(Context context , List<String> meals , HashMap<String,List<String>> mealItems){
-        this.meals = meals;
-        this.mealItems = mealItems;
+    public ExpandableListViewAdapter(Context context, List<String> listDataHeader,
+                                 HashMap<String, List<String>> listChildData) {
         this.context = context;
+        this.meals = listDataHeader;
+        this.mealItems = listChildData;
     }
 
     @Override
-    public int getGroupCount() {
-        return meals.size();
-    }
-
-    @Override
-    public int getChildrenCount(int groupPosition) {
-        return mealItems.get(meals.get(groupPosition)).size();
-    }
-
-    @Override
-    public Object getGroup(int groupPosition) {
-        return meals.get(groupPosition);
-    }
-
-    @Override
-    public Object getChild(int groupPosition, int childPosition) {
-        return mealItems.get(meals.get(groupPosition)).get(childPosition);
-    }
-
-    @Override
-    public long getGroupId(int groupPosition) {
-        return groupPosition;
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this.mealItems.get(this.meals.get(groupPosition))
+                .get(childPosititon);
     }
 
     @Override
@@ -57,34 +38,65 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter{
     }
 
     @Override
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.child_expandable_view, null);
+        }
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.lblListItem);
+
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.mealItems.get(this.meals.get(groupPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.meals.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.meals.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.parent_expandable_view, null);
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
     public boolean hasStableIds() {
         return false;
-    }
-
-    @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        String mealName= (String)this.getGroup(groupPosition);
-        if(convertView != null){
-            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.parent_expandable_view,null);
-            TextView mealTextView = (TextView) convertView.findViewById(R.id.mealNameTextView);
-            mealTextView.setTypeface(null,Typeface.BOLD);
-            mealTextView.setText(mealName);
-        }
-        return convertView;
-    }
-
-    @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        String mealItemName = (String) this.getChild(groupPosition,childPosition);
-        if(convertView != null){
-            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.child_expandable_view,null);
-            TextView mealTextView = (TextView) convertView.findViewById(R.id.mealItemTextView);
-            mealTextView.setTypeface(null,Typeface.BOLD);
-            mealTextView.setText(mealItemName);
-        }
-        return convertView;
     }
 
     @Override
--- app/src/main/java/pl/edu/pwr/a200184student/my_personal_trainer/controller/MainPanelCalendarDetailController.java
+++ app/src/main/java/pl/edu/pwr/a200184student/my_personal_trainer/controller/MainPanelCalendarDetailController.java
@@ -4,8 +4,10 @@ package pl.edu.pwr.a200184student.my_personal_trainer.controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ExpandableListView;
-
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,34 +17,109 @@ import pl.edu.pwr.a200184student.my_personal_trainer.R;
 
 public class MainPanelCalendarDetailController extends AppCompatActivity {
 
-    private ExpandableListView mealsExpandableListView ;
-    private List<String> meals;
-    private HashMap<String,List<String>> mealItems;
+    ExpandableListViewAdapter mealsListAdapter;
+    ExpandableListView mealsExpandableList;
+    List<String> mealsList;
+    HashMap<String, List<String>> mealItemsHashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_panel_calendar_detail_view);
         initializeFields();
-
+        prepareListAdapter();
+        setOnItemLongClickListener();
+        prepareListData();
     }
 
     private void initializeFields() {
-        mealsExpandableListView = (ExpandableListView)findViewById(R.id.mealsExpandableListView);
-        meals = new ArrayList<>();
-        List<String> L1 = new ArrayList<>();
-        mealItems = new HashMap<>();
-        String [] mealsArray = getResources().getStringArray(R.array.header_meals_titles);
-        String [] mealsItemArray = getResources().getStringArray(R.array.child_mealItems_titles);
-        for(String meal : mealsArray){
-            meals.add(meal);
-        }
-        for(String mealItem : mealsItemArray){
-            L1.add(mealItem);
-        }
-        mealItems.put(meals.get(0),L1);
-        ExpandableListViewAdapter adapter = new ExpandableListViewAdapter(getApplicationContext(),meals,mealItems);
-        mealsExpandableListView.setAdapter(adapter);
+        mealsExpandableList = (ExpandableListView) findViewById(R.id.mealsExpandableListView);
+        mealsList = new ArrayList<>();
+        mealItemsHashMap = new HashMap<>();
+    }
+
+    private void prepareListAdapter() {
+        mealsListAdapter = new ExpandableListViewAdapter(this, mealsList, mealItemsHashMap);
+        mealsExpandableList.setAdapter(mealsListAdapter);
+    }
+
+    private void setOnItemLongClickListener() {
+        mealsExpandableList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                long packedPosition = mealsExpandableList.getExpandableListPosition(position);
+                int itemType = ExpandableListView.getPackedPositionType(packedPosition);
+                int groupPosition = ExpandableListView.getPackedPositionGroup(packedPosition);
+                int childPosition = ExpandableListView.getPackedPositionChild(packedPosition);
+                if (itemType == ExpandableListView.PACKED_POSITION_TYPE_GROUP) {
+                    onGroupLongClick(groupPosition);
+                }
+                else if (itemType == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
+                    onChildLongClick(groupPosition, childPosition);
+                }
+                return true;
+            }
+            private void onGroupLongClick(int groupPosition) {
+                Toast.makeText(getApplicationContext(), "klikłem na parenta !" , Toast.LENGTH_LONG).show();
+            }
+
+            private void onChildLongClick(int groupPosition, int childPosition) {
+                Toast.makeText(getApplicationContext(), "klikłem na childa" , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void prepareListData() {
+
+        mealsList.add("Śniadanie");
+        mealsList.add("Lunch");
+        mealsList.add("Obiad");
+        mealsList.add("Podwieczorek");
+        mealsList.add("Kolacja");
+
+        List<String> breakfestItems = new ArrayList<String>();
+        breakfestItems.add("item one");
+        breakfestItems.add("item two");
+        breakfestItems.add("item three");
+        breakfestItems.add("item four");
+        breakfestItems.add("item five");
+        breakfestItems.add("item six");
+        breakfestItems.add("item seven");
+
+        List<String> lunchItems = new ArrayList<String>();
+        lunchItems.add("item one");
+        lunchItems.add("item two");
+        lunchItems.add("item three");
+        lunchItems.add("item four");
+        lunchItems.add("item five");
+        lunchItems.add("item six");
+
+        List<String> dinnerItems = new ArrayList<String>();
+        dinnerItems.add("item one");
+        dinnerItems.add("item two");
+        dinnerItems.add("item three");
+        dinnerItems.add("item four");
+        dinnerItems.add("item five");
+
+        List<String> teaItems = new ArrayList<String>();
+        teaItems.add("item one");
+        teaItems.add("item two");
+        teaItems.add("item three");
+        teaItems.add("item four");
+        teaItems.add("item five");
+
+        List<String> supperItems = new ArrayList<String>();
+        supperItems.add("item one");
+        supperItems.add("item two");
+        supperItems.add("item three");
+        supperItems.add("item four");
+        supperItems.add("item five");
+
+        mealItemsHashMap.put(mealsList.get(0), breakfestItems); // Header, Child data
+        mealItemsHashMap.put(mealsList.get(1), lunchItems);
+        mealItemsHashMap.put(mealsList.get(2), dinnerItems);
+        mealItemsHashMap.put(mealsList.get(3),teaItems);
+        mealItemsHashMap.put(mealsList.get(4),supperItems);
     }
 
     @Override
